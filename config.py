9653e5caa81effba23f8c0c78a1d100bf1c13819
Alf Mikael Constantinou <amikaelc@protonmail.com>
2021-08-02 14:31:30 +0200

    Changed from using os.path to pathlib

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
-import os.path
+from pathlib import Path
 
 app_name = 'snap generator'
 company_name = "Alf Mikael Constantinou"
 
-
 # ***Ignore Below this line unless you are sure***
 lib_dir = 'lib'
-app_path = os.path.dirname(os.path.abspath(__file__))
-lib_path = os.path.join(app_path, lib_dir, '')
+app_path = Path(__file__).parent
+lib_path = app_path / lib_dir
