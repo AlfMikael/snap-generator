3c0f037b0939c64d420d108dde7a0e5c69a0b18d
Alf Mikael Constantinou <amikaelc@protonmail.com>
2023-09-15 12:12:55 +0200

    Added return  functionality

--- commands/test_lib/__init__.py
+++ commands/test_lib/__init__.py
@@ -21,10 +21,11 @@ BASE_PARAMETERS["name"] = "default_cantilever"
 
 
 
+
 def create_cantilever(modified_parameters=dict()):
     p = BASE_PARAMETERS.copy()
     p.update(modified_parameters)
-    
+
     # Multiplying all values by 10 to fix weirdness
     th = p["thickness"]
     l = p["length"]
@@ -67,23 +68,24 @@ def create_cantilever(modified_parameters=dict()):
     assy = cq.Assembly()
     assy.add(sketch, name="sketch", color=cq.Color("gray"))
     cq.exporters.export(sketch, name + ".step")
-    
+
     # Open stepfile to edit
-    from steputils import  p21
+    from steputils import p21
     fname = name + ".step"
     stepfile = p21.readfile(fname)
-    
+
     product = stepfile.data[0].get("#7")
     product.entity.params = (name,)
-    
+
     stepfile.save(fname)
-    
+
+
 
 def import_step_file(modified_parameters=dict()):
-    
+
     p = modified_parameters
     name = p["name"]
-    
+
     # Import part to Fusion 360
     ui = None
     try:
@@ -105,22 +107,59 @@ def import_step_file(modified_parameters=dict()):
         stpOptions.isViewFit = False
 
         # Import step file to root component
-        importManager.importToTarget2(stpOptions, rootComp)
+        imported_comp = importManager.importToTarget2(stpOptions, rootComp)
+        return imported_comp
+
+
     except:
-        # Allowing silent error here because of bug
-        pass
         if ui:
             ui.messageBox('Import step file Failed:\n{}'.format(traceback.format_exc()))
-    
+
+
+def import_step_file2(modified_parameters=dict()):
+
+    p = modified_parameters
+    name = p["name"]
+
+    # Import part to Fusion 360
+    ui = None
+    imported_object = None
+    try:
+        app = adsk.core.Application.get()
+        ui = app.userInterface
+        # Get import manager
+        importManager = app.importManager
+
+        # Get active design
+        product = app.activeProduct
+        design = adsk.fusion.Design.cast(product)
+
+        # Get root component
+        rootComp = design.rootComponent
+
+        # Get step import options
+        stpFileName = name + ".step"
+        stpOptions = importManager.createSTEPImportOptions(stpFileName)
+        stpOptions.isViewFit = False
+
+        # Import step file to root component
+        imported_object = importManager.importToTarget2(stpOptions, rootComp)
+        return imported_object
+    except:
+        if ui:
+            ui.messageBox('Import step file Failed:\n{}'.format(traceback.format_exc()))
+            #ui.messageBox("object:" + str(imported_object.asArray()))
+
 
 def get_cantilever(modified_parameters=dict()):
     p = BASE_PARAMETERS.copy()
     p.update(modified_parameters)
     # First create a new cantilever with given parameters
 
-    
+
     create_cantilever(modified_parameters)
-    import_step_file(p)
-    
+    imported_object = import_step_file(p)
+    return imported_object
+
 
 
