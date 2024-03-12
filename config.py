8552288d740de23446cee17a32b8a21e539abefc
Alf Mikael Constantinou <amikaelc@protonmail.com>
2024-03-12 15:10:17 +0100

    Fixed typo in name of app

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
-app_name = 'snap generator'
+app_name = 'snap-generator'
 company_name = "Alf Mikael Constantinou"
 
-# ***Ignore Below this line unless you are sure***
 lib_dir = 'lib'
 app_path = Path(__file__).parent
 lib_path = app_path / lib_dir
