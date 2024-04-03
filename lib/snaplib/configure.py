2f11b7c4e379d0ba9bc62348a44687f1d3653031
Alf Mikael Constantinou <amikaelc@protonmail.com>
2024-04-03 20:29:34 +0200

    Improved profile data I/O

--- lib/snaplib/configure.py
+++ lib/snaplib/configure.py
@@ -1,5 +1,6 @@
 import json
 import os
+import traceback
 
 from .. import appdirs
 
@@ -22,16 +23,36 @@ CONFIG_PATH = Path(appdirs.user_config_dir(appname=APPNAME, version=VERSION))
 LOGS_PATH = Path(appdirs.user_log_dir(appname=APPNAME, version=VERSION))
 SETTINGS_PATH = CONFIG_PATH / "settings.json"
 
+# For debugging
+import adsk.core
+app = adsk.core.Application.get()
+ui = app.userInterface
+# ui.messageBox("Opened configure.py")
+
+
 config = None
 def set_config(c):
     global config
     config = c
 
-def reset_config_file(commandName: str):
-    source_config_file_path = config.app_path / "default_config" / f"{commandName}.json"
-    target_config_file_path = CONFIG_PATH / f"{commandName}.json"
+def reset_single_profile_data(command_name: str):
+    try:
+        if command_name not in CONFIGURABLE_COMMANDS:
+            raise KeyError(f"\"{command_name}\" not in list of configurable commands" )
 
-    os.makedirs(CONFIG_PATH, exist_ok=True)
+        else:
+            source_config_file_path = config.app_path / "default_config" / f"{command_name}.json"
+            target_config_file_path = CONFIG_PATH / "ProfileData" / f"{command_name}.json"
+            os.makedirs(CONFIG_PATH / "ProfileData", exist_ok=True)
+            shutil.copy(source_config_file_path, target_config_file_path)
+    except:
+        ui.messageBox(traceback.format_exc())
+def reset_all_profile_data():
+    os.makedirs(CONFIG_PATH / "ProfileData", exist_ok=True)
+    for command_name in CONFIGURABLE_COMMANDS:
+        source_config_file_path = config.app_path / "default_config" / f"{command_name}.json"
+        target_config_file_path = CONFIG_PATH / "ProfileData" / f"{command_name}.json"
+        shutil.copy(source_config_file_path, target_config_file_path)
 
 def get_settings():
     # First check that the settings file exists
@@ -55,11 +76,24 @@ def reset_settings():
     ui = app.userInterface
 
     os.makedirs(str(CONFIG_PATH), exist_ok=True)
-    ui.messageBox(f"{str(CONFIG_PATH)}")
+    # ui.messageBox(f"{str(CONFIG_PATH)}")
 
     shutil.copy(source_settings_file_path, target_settings_file_path)
 
+def reset_profile_config(command_name):
+    """ Replace a profile config file for the given command, with the default."""
+    if (command_name == "CantileverCommand"):
+        shutil.copy()
+
+    elif (command_name == "CantileverPinCommand"):
+        pass
+
+
+
 def reset_all_config():
+    for command_name in CONFIGURABLE_COMMANDS:
+        reset_profile_config(command_name)
+
     source_config_folder = config.app_path / "default_config"
     destination_config_folder = CONFIG_PATH
 
