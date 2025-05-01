d2b886666ded813b07f33213d8fac3cd5f69fcac
Alf Mikael Constantinou <amikaelc@protonmail.com>
2025-05-01 14:49:09 +0200

    Cleaned up some of the messy code in configure.py.

--- lib/snaplib/configure.py
+++ lib/snaplib/configure.py
@@ -1,6 +1,8 @@
-import os, json, traceback
+import adsk.core
 
-# from .. import appdirs
+import os
+import json
+import traceback
 from pathlib import Path
 import shutil
 
@@ -18,13 +20,8 @@ CONFIG_PATH = app_path / "config"
 LOGS_PATH = app_path / "logs"
 SETTINGS_PATH = CONFIG_PATH / "settings.json"
 
-# For debugging
-import adsk.core
 app = adsk.core.Application.get()
 ui = app.userInterface
-# ui.messageBox("Opened configure.py")
-
-
 config = None
 def set_config(c):
     global config
@@ -42,6 +39,7 @@ def reset_single_profile_data(command_name: str):
             shutil.copy(source_config_file_path, target_config_file_path)
     except:
         ui.messageBox(traceback.format_exc())
+
 def reset_all_profile_data():
     os.makedirs(CONFIG_PATH / "ProfileData", exist_ok=True)
     for command_name in CONFIGURABLE_COMMANDS:
@@ -60,43 +58,9 @@ def dump_settings(settings_dict):
     with open(SETTINGS_PATH, "w") as f:
         return json.dump(settings_dict, f, indent=4)
 
-
 def reset_settings():
     source_settings_file_path = config.app_path / "default_config" / f"settings.json"
     target_settings_file_path = SETTINGS_PATH
-    destination_dir = os.path.dirname(CONFIG_PATH)
-
-    import adsk.core
-    app = adsk.core.Application.get()
-    ui = app.userInterface
-
     os.makedirs(str(CONFIG_PATH), exist_ok=True)
-    # ui.messageBox(f"{str(CONFIG_PATH)}")
-
     shutil.copy(source_settings_file_path, target_settings_file_path)
-
-def reset_profile_config(command_name):
-    """ Replace a profile config file for the given command, with the default."""
-    if (command_name == "CantileverCommand"):
-        shutil.copy()
-
-    elif (command_name == "CantileverPinCommand"):
-        pass
-
-
-
-def reset_all_config():
-    for command_name in CONFIGURABLE_COMMANDS:
-        reset_profile_config(command_name)
-
-    source_config_folder = config.app_path / "default_config"
-    destination_config_folder = CONFIG_PATH
-
-    if destination_config_folder.exists:
-        shutil.rmtree(destination_config_folder)
-
-    os.makedirs(CONFIG_PATH, exist_ok=True)
-    shutil.copytree(source_config_folder, destination_config_folder)
-
-
 
